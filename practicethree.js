404e0b322bbd653a6fd0a8e521735b577670bb25
Tia Low <lowtia@gmail.com>
2020-05-21 08:07:50 -0700

    new js file and updated html for script

---
+++ practicethree.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// This script changes the Seattle question when clicked.
+
+<script>
+    function myFunction() {
+        document.getElementById("demo").innerHTML = "All good! There are other options.";
+        }
+</script>
+
