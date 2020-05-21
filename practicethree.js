4678ef4d6178e913d4b0eb681afeac7286401ed4
Tia Low <lowtia@gmail.com>
2020-05-21 08:15:17 -0700

    removed script for js file so it works now

--- practicethree.js
+++ practicethree.js
@@ -2,9 +2,7 @@
 
 // This script changes the Seattle question when clicked.
 
-<script>
     function myFunction() {
         document.getElementById("jschange").innerHTML = "All good! There are other options.";
         }
-</script>
 
