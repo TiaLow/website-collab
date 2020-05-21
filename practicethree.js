b1066b5f182dc876cf53cca9ea0bdb4ecbc7958a
Tia Low <lowtia@gmail.com>
2020-05-21 15:15:54 -0700

    removed two unnecessary js files and changed name of remaining one

--- practicethree.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-// This script changes the Seattle question when clicked.
-
-    function myFunction() {
-        document.getElementById("jschange").innerHTML = "All good! There are other options.";
-        }
-
