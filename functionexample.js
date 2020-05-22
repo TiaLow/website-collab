0d2b16536d219102f1bf743ff23efb605f398da0
Tia Low <lowtia@gmail.com>
2020-05-21 22:34:36 -0700

    started js from scratch added function

--- functionexample.js
+++ functionexample.js
@@ -2,36 +2,21 @@
 
 // Do over! Starting from scratch here
 
-function askName() {
-    var userName = prompt('Hello.  What is your name?');
-    console.log(userName);
-    return userName;
-}
 
-function askOldComputer(userName){
-    var oldComputer = prompt(', Do you like old computers?').toLowerCase();
-    return oldComputer;
+function askAge() {
+    var age = prompt('Hello. How old are you?');
+    console.log(age);
+    return age;
 }
-
-function typeOfComputer(){
-    var typeComputer = prompt('What type of computer do you have?');
-    console.log(typeComputer);
-}
-
-function likeOldComputers(oldComputerFromFunction){
-    if (oldComputerFromFunction === 'yes'){
-        alert('Awesome.  I like old computers too!');
-    } else if (oldComputerFromFunction === 'no'){
-        alert('How can I change you mind about that?');
+function neverTooOld(ageFromFunction){
+    if (ageFromFunction < 100){
+    alert('Awesome! Being younger than 100 means now is a good time to start!');
+    } else if (ageFromFunction === 100){
+    alert('Awesome! You are 100!');
+    } else if (ageFromFunction > 100){
+    alert('Congrats, you are older than 100.');
     }
 }
 
-
-if (typeOfComputerFromFunction === 'vic-20'){
-        alert('Wow, that is really old');
-    }
-    
-    var userNameFromFunction = askName();
-    var oldComputerFromFunction = askOldComputer(userNameFromFunction);
-    var typeOfComputerFromFunction = typeOfComputer();
-    likeOldComputers(oldComputerFromFunction)
+var ageFromFunction = askAge();
+neverTooOld(ageFromFunction)
