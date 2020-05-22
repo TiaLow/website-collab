7631e695e8b5317c77104fea12ec69849fd33bcd
Tia Low <lowtia@gmail.com>
2020-05-21 18:21:19 -0700

    cant remember the updated but im gonna start from scratch anyway so nbd

--- functionexample.js
+++ functionexample.js
@@ -8,14 +8,8 @@ function askName() {
     return userName;
 }
 
-function askOldComputer(potatoe){
-    var oldComputer = prompt(potatoe + ', Do you like old computers?').toLowerCase();
-    console.log('before the while ' + oldComputer);
-    while((oldComputer !== 'no') && (oldComputer !== 'yes')){
-        console.log('in while but before re-assign ' + oldComputer);
-        oldComputer = prompt(potatoe + ', Do you like old computers?').toLowerCase();
-        console.log('in while but after re-assign ' + oldComputer);
-    }
+function askOldComputer(userName){
+    var oldComputer = prompt(', Do you like old computers?').toLowerCase();
     return oldComputer;
 }
 
@@ -28,7 +22,7 @@ function likeOldComputers(oldComputerFromFunction){
     if (oldComputerFromFunction === 'yes'){
         alert('Awesome.  I like old computers too!');
     } else if (oldComputerFromFunction === 'no'){
-        alert('How can I cahnge you mind about that?');
+        alert('How can I change you mind about that?');
     }
 }
 
@@ -41,18 +35,3 @@ if (typeOfComputerFromFunction === 'vic-20'){
     var oldComputerFromFunction = askOldComputer(userNameFromFunction);
     var typeOfComputerFromFunction = typeOfComputer();
     likeOldComputers(oldComputerFromFunction)
-
-    var letterAnswer = 'a'
-    var guess = 5;
-    var letterGuess = prompt('Guess a letter (single letter only)');
-
-    for (var i = 0; i < guess; i = i + 1){
-        if (letterGuess === letterAnswer){
-            alert('DING DING DING.  You got it right');
-            break;
-        } else {
-            var left = guess - i;
-            letterGuess = prompt('Try Again.  Please enter another letter.  ' + left + ' left.');
-        }
-    }
-    
