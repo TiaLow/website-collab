0d691b2f1020b52813901b8a465c3dbc7a0da1ba
Tia Low <lowtia@gmail.com>
2020-05-22 08:16:34 -0700

    trying to make loops work

--- functionexample.js
+++ functionexample.js
@@ -1,10 +1,10 @@
 'use strict'
 
-// Do over! Starting from scratch here
+// The two functions below prompt for age and based on your answer of less than, equal to, or more than 100, it will return a different message. 
 
 
 function askAge() {
-    var age = prompt('Hello. How old are you?');
+    var age = prompt(' How old are you?');
     console.log(age);
     return age;
 }
@@ -20,3 +20,51 @@ function neverTooOld(ageFromFunction){
 
 var ageFromFunction = askAge();
 neverTooOld(ageFromFunction)
+
+// At this point, everything above is working.
+
+function askName(){
+    var userName = prompt('Hello! Whats your name?');
+    return userName;
+}
+
+function askReady(userName){
+    var ready = prompt(userName + ' , are you ready to start coding?').toLowerCase();
+    while((ready !== 'no') && (ready !== 'yes')){
+        ready = prompt(userName + ' , but...but are you ready to start coding?').toLowerCase();
+    }
+    return ready;
+}
+
+var userNameFromFunction = askName();
+
+
+
+// function askDogs(userName){
+//     var askDogs = prompt(userName + ', do you like dogs?') .toLowerCase();
+//     while((askDogs != 'yes') || (askDogs == 'no')){
+//         askDogs = alert('I HOPE YOU BURN IN HELL       .... just kidding !!!') 
+//         askDogs = prompt(userName + ', TRY AGAIN ya dingus! do you like dogs?')
+//     }
+//     return askDogs;
+// }
+
+// var userNameFromFunction = askName();
+// var askDogsFromFunction = askDogs(userNameFromFunction);
+// likeDogs(askDogsFromFunction)
+
+
+
+// var letterAnswer = 'beagles'
+// var guess = 2;
+// var letterGuess = prompt('Which Breed is the friendliest? Bulldogs, Terriers, or Beagles?').toLowerCase();
+    
+// for (var i = 0; i < guess; i = i + 1){
+//     if (letterGuess === letterAnswer){
+//         alert('WELCOME TO THE LAND OF DOG');
+//         break;
+//     } else {
+//         var left = guess - i;
+//         letterGuess = prompt('NOPE. NOPE. Bulldogs, Terriers, or Beagles?  ' + left + ' left.').toLowerCase();
+//     }
+// }
