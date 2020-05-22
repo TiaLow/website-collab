302c4066c2b7df937a65b1d5ab4c5fbaab942197
Tia Low <lowtia@gmail.com>
2020-05-22 08:56:44 -0700

    working loops booyah

--- functionexample.js
+++ functionexample.js
@@ -4,7 +4,7 @@
 
 
 function askAge() {
-    var age = prompt(' How old are you?');
+    var age = prompt('Hello! How old are you?');
     console.log(age);
     return age;
 }
@@ -21,50 +21,22 @@ function neverTooOld(ageFromFunction){
 var ageFromFunction = askAge();
 neverTooOld(ageFromFunction)
 
-// At this point, everything above is working.
-
 function askName(){
-    var userName = prompt('Hello! Whats your name?');
+    var userName = prompt('What is your name?');
     return userName;
 }
 
 function askReady(userName){
-    var ready = prompt(userName + ' , are you ready to start coding?').toLowerCase();
-    while((ready !== 'no') && (ready !== 'yes')){
-        ready = prompt(userName + ' , but...but are you ready to start coding?').toLowerCase();
+    var askReady = prompt(userName + ', are you ready to start coding?') .toLowerCase();
+    while((askReady != 'no') || (askReady != 'yes')){
+       askReady = prompt(userName + ', but...but are you ready to start coding?') .toLowerCase();
     }
-    return ready;
+    return askReady;
 }
 
 var userNameFromFunction = askName();
+var askReadyFromFunction = askReady(userNameFromFunction);
 
 
+// At this point, everything above is working.
 
-// function askDogs(userName){
-//     var askDogs = prompt(userName + ', do you like dogs?') .toLowerCase();
-//     while((askDogs != 'yes') || (askDogs == 'no')){
-//         askDogs = alert('I HOPE YOU BURN IN HELL       .... just kidding !!!') 
-//         askDogs = prompt(userName + ', TRY AGAIN ya dingus! do you like dogs?')
-//     }
-//     return askDogs;
-// }
-
-// var userNameFromFunction = askName();
-// var askDogsFromFunction = askDogs(userNameFromFunction);
-// likeDogs(askDogsFromFunction)
-
-
-
-// var letterAnswer = 'beagles'
-// var guess = 2;
-// var letterGuess = prompt('Which Breed is the friendliest? Bulldogs, Terriers, or Beagles?').toLowerCase();
-    
-// for (var i = 0; i < guess; i = i + 1){
-//     if (letterGuess === letterAnswer){
-//         alert('WELCOME TO THE LAND OF DOG');
-//         break;
-//     } else {
-//         var left = guess - i;
-//         letterGuess = prompt('NOPE. NOPE. Bulldogs, Terriers, or Beagles?  ' + left + ' left.').toLowerCase();
-//     }
-// }
